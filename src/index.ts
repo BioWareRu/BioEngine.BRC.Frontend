a49fbe524a5fda88ea942e6c15441c9fcd66b387
George <sonicgd@gmail.com>
2019-05-21 16:04:03 +0500

    first release

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+require('mdbootstrap/js/bootstrap');
+require('mdbootstrap/js/mdb');
+require('wow.js/dist/wow');
+import WOW = require('wow.js/dist/wow');
+
+new WOW().init();
+
+require('./blocks/gallery');
+require('./blocks/youtube');
+require('./blocks/twitter');
+require('./blocks/twitch');
+
+
