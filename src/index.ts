2b5456b2d63ff89d9ac03e59947238a7e274a9cb
George <sonicgd@gmail.com>
2019-07-08 14:32:09 +0500

    fix mdbootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 require('mdbootstrap/js/bootstrap');
 require('mdbootstrap/js/mdb');
 require('wow.js/dist/wow');
+import bsCustomFileInput from 'bs-custom-file-input';
+(<any>global).bsCustomFileInput = bsCustomFileInput;
 import WOW = require('wow.js/dist/wow');
 
 new WOW().init();
