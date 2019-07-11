83f0bb07bd0b43c74c5c9239bf1e5cacfe41a31a
George <sonicgd@gmail.com>
2019-07-11 15:04:12 +0500

    reduce code

--- src/blocks/twitter.ts
+++ src/blocks/twitter.ts
@@ -1,34 +1,38 @@
-$(function () {
-    window['twttr'] = (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            t = window['twttr'] || {};
-        if (fjs.parentNode === null) {
-            return;
-        }
-        if (d.getElementById(id)) {
-            return t;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://platform.twitter.com/widgets.js';
-        fjs.parentNode.insertBefore(js, fjs);
-
-        t._e = [];
-        t.ready = function (f) {
-            t._e.push(f);
-        };
-
+window['twttr'] = (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window['twttr'] || {};
+    if (fjs.parentNode === null) {
+        return;
+    }
+    if (d.getElementById(id)) {
         return t;
-    }(document, 'script', 'twitter-wjs'));
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js, fjs);
 
-    const tweets = $('.twitter-tweet');
+    t._e = [];
+    t.ready = function (f) {
+        t._e.push(f);
+    };
 
-    $(tweets).each(function (t, tweet) {
+    return t;
+}(document, 'script', 'twitter-wjs'));
 
-        const id = $(tweet).data('tweet-id');
+const tweets = document.getElementsByClassName('twitter-tweet');
+
+for (const key in tweets) {
+    if (!tweets.hasOwnProperty(key)) {
+        continue;
+    }
+
+    const tweet = <HTMLElement>tweets[key];
+    const tweetId = tweet.getAttribute('dataTweetId');
+    if (tweetId) {
         twttr.ready(tw => {
             tw.widgets.createTweet(
-                id, tweet,
+                tweetId, tweet,
                 {
                     conversation: 'all',    // or all
                     cards: 'visible',  // or visible
@@ -36,8 +40,8 @@ $(function () {
                     theme: 'light', // or dark
                     align: 'center'
                 }).then(() => {
-                    $(tweet).addClass('loaded');
+                    tweet.classList.add('loaded');
                 });
         });
-    });
-});
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 require('mdbootstrap/js/bootstrap');
-require('mdbootstrap/js/mdb');
-require('wow.js/dist/wow');
-import bsCustomFileInput from 'bs-custom-file-input';
-(<any>global).bsCustomFileInput = bsCustomFileInput;
-import WOW = require('wow.js/dist/wow');
-
-new WOW().init();
+require('mdbootstrap/js/modules/scrolling-navbar');
+require('mdbootstrap/js/modules/jquery.easing');
+require('mdbootstrap/js/modules/waves');
 
 require('./blocks/gallery');
 require('./blocks/picture');
 require('./blocks/twitter');
-require('./blocks/twitch');
